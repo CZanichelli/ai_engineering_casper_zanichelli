839a1bb58b94f02b17a2ac969bc02aa568ab6a97
Casper Zanichell <casperzanichelli98@gmail.com>
2025-09-30 10:10:10 +0200

    read api

--- code-alongs/10_fastapi_ml_modell/api.py
+++ code-alongs/10_fastapi_ml_modell/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from data_processing import IrisData, IrisInput, PredictionOutput
 import pandas as pd
 import joblib
-from constants import MODEL_PATH
+from constants import MODELS_PATH
 
 app = FastAPI()
 
@@ -15,6 +15,6 @@ def read_data():
 @app.post("/api/predict", response_model=PredictionOutput)
 def predict_flower(payload: IrisInput):
     data_to_predict = pd.DataFrame([payload.model_dump()])
-    clf = joblib.load(MODEL_PATH / "iris_classifier.joblib")
+    clf = joblib.load(MODELS_PATH / "iris_classifier.joblib")
     prediction = clf.predict(data_to_predict)
     return {"predicted_flower": prediction [0]}
